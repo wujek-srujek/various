3822fee7141f44e9ece3e369965d0e9ad5c2814d
Wujek Srujek <wujek.srujekl@gmail.com>
2018-08-17 14:08:59 +0200

    Anki hacking - info about of percentage calculation

--- ankihacks.py
+++ ankihacks.py
@@ -1,5 +1,7 @@
 # run with PYTHONPATH=/usr/share/anki (i)python2
 
+---
+
 # remove reverse cards
 
 from anki.storage import Collection
@@ -21,6 +23,7 @@ for noteId in col.findNotes(''):
 col.close()
 
 ---
+
 # add reverse card with scheduling information of the forward card
 # the usual way would schedule it as new
 
@@ -61,6 +64,7 @@ for row in [list(r) for r in col.db.execute('''
 col.close()
 
 ---
+
 # create a new note with a single card
 
 from anki.storage import Collection
@@ -81,3 +85,78 @@ note['Add Reverse'] = 'y'
 note.flush()
 
 col.close()
+
+---
+
+# review day percentage calculation reverse engineering
+# result: Anki takes the first ever day of review as a number of how many days
+# ago it was and total number of days with reviews and uses that
+# this means days without any scheduled review will influence the stats, not
+# sure if this is good
+# to fake a review for a day just move from other day a review in the revlog
+# table - set the id to the timestamp in millis of the faked day
+
+from anki.storage import Collection
+import types
+
+col = Collection('/Users/rafal/Playground/collection.anki2')
+did = col.decks.byName('Español')['id']
+col.decks.select(did)
+
+stats = col.stats()
+stats.type = 2 # lifetime
+
+def daysStudiedSql(self):
+    # copied from _daysStudied to just show the SQL, not execute it
+    lims = []
+    num = self._periodDays()
+    if num:
+        lims.append(
+            "id > %d" % ((self.col.sched.dayCutoff-(num*86400))*1000))
+    rlim = self._revlogLimit()
+    if rlim:
+        lims.append(rlim)
+    if lims:
+        lim = "where " + " and ".join(lims)
+    else:
+        lim = ""
+    return "select count(), abs(min(day)) from (select (cast((id/1000 - {cut}) / 86400.0 as int)+1) as day from revlog {lim} group by day order by day)".format(lim=lim, cut=self.col.sched.dayCutoff)
+
+stats._daysStudiedSql = types.MethodType(daysStudiedSql, stats)
+stats._daysStudiedSql()
+period = stats._deckAge('review')
+(daysStud, fstDay) = stats._daysStudied()
+daysStud, fstDay, period
+
+"%(pct)d%% (%(x)s of %(y)s)" % dict(x=daysStud, y=period, pct=daysStud/float(period)*100)
+
+col.close()
+
+# col.crt is creation timestamp
+# sched.today is day number after creation
+# sched.dayCutoff is end of current day
+
+# select id from cards where did in (1435088749676)
+# - returns all cards for deck Español
+
+# select * from revlog where cid in (select id from cards where did in (1435088749676))
+# - returns all reviews on all days for all cards for deck Español
+
+# revlog.id is time.time() of review * 1000 (millis)
+
+# select (cast((id/1000 - 1534543200 # cutoff #) / 86400.0 as int)+1) as day from revlog where cid in
+#     (select id from cards where did in (1435088749676))
+# - returns all days with reviews for all cards for deck Español
+# - days 1 for today, 0 for yesterday, negative for more past days
+
+# select (cast((id/1000 - 1534543200 # cutoff #) / 86400.0 as int)+1) as day from revlog where cid in
+#     (select id from cards where did in (1435088749676))
+# group by day order by day
+# - returns distinct days with reviews for all cards for deck Español
+# - days 1 for today, 0 for yesterday, negative for more past days
+
+# select count(), abs(min(day)) from
+#     (select (cast((id/1000 - 1534543200 # cut #) / 86400.0 as int)+1) as day from revlog where cid in
+#         (select id from cards where did in (1435088749676))
+#     group by day order by day)
+# - returns count of all days with any reviews and how many days ago the first review was done (when reviewing started)
